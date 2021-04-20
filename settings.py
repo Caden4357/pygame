b1c126ea46cd2a9c4e8f08612ef10226617702e8
Caden <wilcox4357@gmail.com>
2021-04-20 11:27:19 -0700

    game files

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings():
+    # A class to store game settings
+    def __init__(self):
+        #initialize game settings and screen settings
+        self.screen_width = 1200
+        self.screen_height = 600
+        self.bg_color = (135,206,250)
